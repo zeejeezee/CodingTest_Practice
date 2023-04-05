4dd5832aef1de5495b8e033adc8410cb2c4148e7
zeejeezee <jeezeedev@gmail.com>
2023-04-05 23:39:10 +0900

    4월 5일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex8.java
+++ src/main/java/programmers/lv0/ex8.java
@@ -133,7 +133,22 @@ public class ex8 {
         long answer = Long.parseLong(numbers);
         return answer;
     }
-
+    
+    
+    //잘라서 배열로 저장하기
+    //문자열 my_str과 n이 매개변수로 주어질 때,
+    //my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해 주세요.
+    
+    public String[] solution6(String my_str, int n) {
+        List<String> answer = new ArrayList<>();
+        for (int i = 0; i < (my_str.length() / n); i++) {
+            answer.add(my_str.substring((n * i), (n * (i + 1))));
+        }
+        if (my_str.length() % n != 0) {
+             answer.add(my_str.substring( (my_str.length() / n) * n));
+        }
+        return answer.toArray(new String[0]);
+    }
 
     
 }
