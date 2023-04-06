d534a4542fd2c6fa2127c04f51b8fe712bcb3a58
zeejeezee <jeezeedev@gmail.com>
2023-04-06 23:55:06 +0900

    4월 6일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex8.java
+++ src/main/java/programmers/lv0/ex8.java
@@ -149,6 +149,28 @@ public class ex8 {
         }
         return answer.toArray(new String[0]);
     }
+    
+    
+    //문자열 계산하기
+    //my_string은 "3 + 5"처럼 문자열로 된 수식입니다.
+    //문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해 주세요.
+
+    public int solution7(String my_string) {
+        
+        String[] strArr = my_string.split(" ");
+        int answer = Integer.parseInt(strArr[0]);
+        
+        for (int i = 1; i < strArr.length; i+=2) {
+            if (strArr[i].equals("+")) {
+                answer += Integer.parseInt(strArr[i + 1]);
+            }
+            if (strArr[i].equals("-")) {
+                answer -= Integer.parseInt(strArr[i + 1]);
+            }
+        }
+        
+        return answer;
+    }
 
     
 }
