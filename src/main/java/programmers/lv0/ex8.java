1725654f9bc6fae07c3b10cb2444f4736a5c2541
zeejeezee <jeezeedev@gmail.com>
2023-04-07 17:06:56 +0900

    4월 7일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex8.java
+++ src/main/java/programmers/lv0/ex8.java
@@ -171,6 +171,40 @@ public class ex8 {
         
         return answer;
     }
+    
+    //구슬을 나누는 경우의 수
+    //머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다.
+    //머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때,
+    //balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해 주세요.
+    
+    //구글링 참고
+    
+    public int combination(int n, int r) {
+        if (r == 0 || n == r) {
+            return 1; 
+        } else {
+            return combination(n-1, r-1) + combination(n-1, r);
+        }
+    }
+    
+    public int solution8(int balls, int share) {
+        return combination(balls, share);
+    }
+    
+    //다른 사람 풀이
+    
+    public long solution8_1(int balls, int share) {
+        share = Math.min(balls - share, share);
+
+        if (share == 0)
+            return 1;
+
+        long result = solution8_1(balls - 1, share - 1);
+        result *= balls;
+        result /= share;
+
+        return result;
+    }
 
     
 }
