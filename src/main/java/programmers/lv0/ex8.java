27b4b2e4cfd60c17ebe442cb6acce344fe82e800
zeejeezee <jeezeedev@gmail.com>
2023-04-10 23:48:19 +0900

    4월 10일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex8.java
+++ src/main/java/programmers/lv0/ex8.java
@@ -222,6 +222,28 @@ public class ex8 {
         
         return highValue - lowValue - 1;
     }
+    
+    
+    //외계어 사전
+    //PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다.
+    //알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
+    //spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1,
+    //존재하지 않는다면 2를 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution10(String[] spell, String[] dic) {
+        boolean wordCheck = false;
+        for (String word : dic) {
+            int count = 0;
+            for (String s : spell) {
+                if (word.contains(s)) count++;
+            }
+            if (count == spell.length) {
+                wordCheck = true;
+                break;
+            }
+        }
+        return wordCheck ? 1 : 2;
+    }
 
 
 
