267e0a688c6410901d9b8d7876e8c3a7a679585e
zeejeezee <jeezeedev@gmail.com>
2023-04-14 21:57:31 +0900

    4월 14일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex9.java
+++ src/main/java/programmers/lv0/ex9.java
@@ -53,6 +53,26 @@ public class ex9 {
         
         return answer;
     }
+    
+    
+    //직사각형 넓이 구하기
+    //2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다.
+    //직사각형 네 꼭짓점의 좌표 [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가 담겨있는 배열 dots가 매개변수로 주어질 때,
+    //직사각형의 넓이를 return 하도록 solution 함수를 완성해 보세요.
+    
+    public int solution3(int[][] dots) {
+        int width = 0;
+        int height = 0;
+        int x = dots[0][0];
+        int y = dots[0][1];
+        
+        for (int i = 1; i < dots.length; i++) {
+            if (x != dots[i][0]) width = Math.abs(x - dots[i][0]);
+            if (y != dots[i][1]) height = Math.abs(y - dots[i][1]);
+        }
+        
+        return width * height;
+    }
 
 
 
