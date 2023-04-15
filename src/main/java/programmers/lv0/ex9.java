49e72762c9be02b3240e5f9688e5680324bf2cb5
zeejeezee <jeezeedev@gmail.com>
2023-04-15 15:37:21 +0900

    4월 15일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex9.java
+++ src/main/java/programmers/lv0/ex9.java
@@ -1,5 +1,8 @@
 package programmers.lv0;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ex9 {
 	
 	//종이 자르기
@@ -72,6 +75,35 @@ public class ex9 {
         }
         
         return width * height;
+    }
+    
+    
+    //로그인 성공
+    //머쓱이는 프로그래머스에 로그인하려고 합니다.
+    //머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때,
+    //다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해 주세요.
+
+    //아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return 합니다.
+    //로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를,
+    //아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+    
+    public String solution4(String[] id_pw, String[][] db) {
+        Map<String, String> memberTable = new HashMap<>();
+        
+        for (int i = 0; i < db.length; i++) {
+            memberTable.put(db[i][0], db[i][1]);
+        }
+        try {
+            if (memberTable.get(id_pw[0]).equals(id_pw[1])) {
+                return "login";
+            } else if (!memberTable.get(id_pw[0]).equals(id_pw[1])) {
+                return "wrong pw";
+            } else {
+                return "fail";
+            }
+        } catch (Exception e) {
+            return "fail";
+        }
     }
 
 
