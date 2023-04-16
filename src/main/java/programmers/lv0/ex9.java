5f47a168e053a95e00d569181703125431f4af93
zeejeezee <jeezeedev@gmail.com>
2023-04-16 23:24:28 +0900

    4월 16일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex9.java
+++ src/main/java/programmers/lv0/ex9.java
@@ -105,6 +105,32 @@ public class ex9 {
             return "fail";
         }
     }
+    
+    
+    //치킨 쿠폰
+    //프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다.
+    //쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다.
+    //시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때
+    //받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution5(int chicken) {
+        
+        int coupon = chicken; //받는 쿠폰
+        int remainCoupon = 0; //쓰고 남은 쿠폰
+        int answer = 0; //서비스 치킨
+        
+        while (coupon + remainCoupon >= 10) {
+            int a = (coupon + remainCoupon) / 10; //몫
+            int b = (coupon + remainCoupon) % 10; //나머지
+            
+            coupon = a;
+            remainCoupon = b;
+            
+            answer += coupon;
+        }
+        
+        return answer;
+    }
 
 
 
