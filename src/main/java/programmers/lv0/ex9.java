78175a2b41ca6c719cf282af60dfaf552bda74ea
zeejeezee <jeezeedev@gmail.com>
2023-04-22 22:07:05 +0900

    4월 22일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex9.java
+++ src/main/java/programmers/lv0/ex9.java
@@ -268,25 +268,44 @@ public class ex9 {
     
     //구글링
     //내일 다시 보기
+	public static int value = 0;
+	
 	public int[] solution9(int[] numlist, int n) {
-		List<Integer> nums = new ArrayList<>();
-		Arrays.sort(numlist);
-		for (int num : numlist) nums.add(num);
-		nums.sort((s1, s2) -> Integer.compare(Math.abs(s2 - n), Math.abs(s1 - n)));
-		Collection.reverse(nums);
-		return nums.stream().mapToInt(num -> num).toArray();
-	}
-
-	public static void main(String[] args) {
-		UniqueSort uniqueSort = new UniqueSort();
-		String output1 = Arrays.toString(uniqueSort.solution(new int[]{1, 2, 3, 4, 5, 6}, 4));
-		System.out.println(output1 + " " + output1.equals("[4, 5, 3, 6, 2, 1]"));
-
-		String output2 = Arrays.toString(uniqueSort.solution(new int[]{10000, 20, 36, 47, 40, 6, 10, 7000}, 30));
-		System.out.println(output2 + " " + output2.equals("[36, 40, 20, 47, 10, 6, 7000, 10000]"));
-
-		String output3 = Arrays.toString(uniqueSort.solution(new int[]{10,9,8,7,6,5,4,3,2,1,11}, 6));
-		System.out.println(output3 + " " + output3.equals("[6, 7, 5, 8, 4, 9, 3, 10, 2, 11, 1]"));
+		int size = numlist.length;
+		value = n;
+		int[] answer = new int[size];
+		boolean[] used = new boolean[size];
+		
+		for(int i = 0; i < size; i++) {
+			int index = -1;
+			for(int j = 0; j < size; j++) {
+				if(used[j]) {
+					continue;
+				}
+				
+				if(index == -1) {
+					index = j;
+					continue;
+				}
+				
+				int prev = numlist[index];
+				int now = numlist[j];
+				
+				if(Math.abs(n-prev) > Math.abs(n-now)) {
+					index = j;
+					continue;
+				}
+				
+				if(Math.abs(n-prev) == Math.abs(n-now) && prev < now) {
+					index = j;
+				}
+				
+				used[index] = true;
+				answer[i] = numlist[index];
+			}
+		}
+		
+		return answer;	
 	}
 
     
