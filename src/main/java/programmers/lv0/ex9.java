0adb0ae10a39edd12addf4af3532d5e1f00c990a
zeejeezee <jeezeedev@gmail.com>
2023-04-25 22:09:03 +0900

    4월 25일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex9.java
+++ src/main/java/programmers/lv0/ex9.java
@@ -327,6 +327,31 @@ public class ex9 {
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
+    
+    
+    //문자열 밀기
+    //문자열 "hello"에서 각 문자를 오른쪽으로 한 칸씩 밀고 마지막 문자는 맨 앞으로 이동시키면 "ohell"이 됩니다.
+    //이것을 문자열을 민다고 정의한다면 문자열 A와 B가 매개변수로 주어질 때,
+    //A를 밀어서 B가 될 수 있다면 밀어야 하는 최소 횟수를 return 하고
+    //밀어서 B가 될 수 없으면 -1을 return 하도록 solution 함수를 완성해 보세요.
+    
+    public int solution10(String A, String B) {
+        int answer = 0;
+        
+        String copyA = A;
+        for(int i = 0; i < A.length(); i++) {
+            if(copyA.equals(B)) {
+                return answer; 
+            }
+            
+            String a = copyA.substring(copyA.length() - 1);
+            copyA = a + copyA.substring(0, copyA.length() - 1);
+            answer++;
+            
+        }
+            
+        return -1;
+    }   
 
     
 }
