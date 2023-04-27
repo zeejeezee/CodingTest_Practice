5b2fe79db5cbc60acb55e3258faa6f83c9f929b8
zeejeezee <jeezeedev@gmail.com>
2023-04-27 23:29:45 +0900

    4월 27일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -38,6 +38,30 @@ public class ex10 {
         return (xNum != 0 ? xNum > 1 ? xNum + "x" : "x" : "") + (num != 0 ? (xNum != 0 ? " + " : "") + num : xNum == 0 ? "0" : "");
         
     }
+    
+    
+    //분수의 덧셈
+    //첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+    //두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해 보세요.
+    
+    public int[] solution2(int numer1, int denom1, int numer2, int denom2) {
+        
+        //최소공배수 : (a * b) / 최대공약수
+        
+        int numerator = (numer1 * denom2) + (numer2 * denom1);
+        int denominator = denom1 * denom2;
+        
+        for(int i = numerator - 1; i > 1; i--) {
+            if(numerator % i == 0 && denominator % i == 0) {
+                numerator /= i;
+                denominator /= i;
+            }
+        }
+        
+        int[] answer = {numerator, denominator};
+        
+        return answer;
+    }
         
 
     
