464ebf4d77266760c7d17996f4fef994bb6831bc
zeejeezee <jeezeedev@gmail.com>
2023-05-01 23:51:40 +0900

    5월 1일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -142,6 +142,23 @@ public class ex10 {
         return answer;
     }
     
+    
+    //다음에 올 숫자
+    //등차수열 혹은 등비수열 common이 매개변수로 주어질 때,
+    //마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해 보세요.
+    
+    public int solution5(int[] common) {
+        int answer = 0;
+        
+        if((common[1] - common[0]) == (common[2] - common[1])) {
+            answer = common[common.length-1] + (common[1] - common[0]);
+        } else {
+            answer = common[common.length-1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+    
         
 
     
