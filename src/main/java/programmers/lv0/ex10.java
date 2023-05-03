5ec922f07a11fa491e18d7e359a6435ebdb05262
zeejeezee <jeezeedev@gmail.com>
2023-05-03 18:18:24 +0900

    5월 3일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -197,6 +197,23 @@ public class ex10 {
         return answer;
     }
     
+    
+    //연속된 수의 합\
+    //연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다.
+    //연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해 보세요.
+    
+    public int[] solution7(int num, int total) {
+        int[] answer = new int[num];
+        int number = (total / num) - ((num - 1) / 2);
+        
+        for (int i = 0; i < num; i++) {
+            answer[i] = number;
+            number++;
+        }
+        
+        return answer;
+    }
+    
         
 
     
