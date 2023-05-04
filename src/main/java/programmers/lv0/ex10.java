9b836735fe6130020d7454b804a61751f69f29a8
zeejeezee <jeezeedev@gmail.com>
2023-05-04 23:12:40 +0900

    5월 4일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -198,7 +198,7 @@ public class ex10 {
     }
     
     
-    //연속된 수의 합\
+    //연속된 수의 합
     //연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다.
     //연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해 보세요.
     
@@ -214,6 +214,38 @@ public class ex10 {
         return answer;
     }
     
+    
+    
+    //안전지대
+    //다음 그림과 같이 지뢰가 있는 지역과 지뢰에 인접한 위, 아래, 좌, 우 대각선 칸을 모두 위험지역으로 분류합니다.
+    //지뢰는 2차원 배열 board에 1로 표시되어 있고 board에는 지뢰가 매설 된 지역 1과, 지뢰가 없는 지역 0만 존재합니다.
+    //지뢰가 매설된 지역의 지도 board가 매개변수로 주어질 때, 안전한 지역의 칸 수를 return하도록 solution 함수를 완성해 주세요.
+    
+    //구글링 참고
+    public int solution8(int[][] board) {
+        int[][] newBoard = new int [board.length][board[0].length];
+        
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == 1) checkExplosion(i, j, newBoard);
+            }
+        }
+        
+        int count = 0;
+        for(int[] array : newBoard) {
+            for(int value : array) {
+                if(value == 0) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    static void checkExplosion(int x, int y, int[][] newBoard) {
+    	
+    }
         
 
     
