607c1843d5d76bb018a4ba7e6e5cfd98a8c4fb23
zeejeezee <jeezeedev@gmail.com>
2023-05-06 21:24:45 +0900

    5월 6일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -292,6 +292,38 @@ public class ex10 {
             }
         }
     }
+    
+    
+    
+    //평행
+    //점 네 개의 좌표를 담은 이차원 배열  dots가 다음과 같이 매개변수로 주어집니다.
+    //[[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+    //주어진 네 개의 점을 두 개씩 이었을 때,
+    //두 직선이 평행이 되는 경우가 있으면 1을 없으면 0을 return 하도록 solution 함수를 완성해 보세요.
+
+    //기울기 구하는 공식 : (y2 - y1) / (x2 - x1) 
+    
+    public double findSlope(int[] dot1, int[] dot2) {
+        return (double) (dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
+    }
+    
+    public int solution9(int[][] dots) {
+        int answer = 0;
+        
+        if (findSlope(dots[0], dots[1]) == findSlope(dots[2], dots[3])) {
+            return 1;
+        }
+        
+        if (findSlope(dots[0], dots[2]) == findSlope(dots[1], dots[3])) {
+            return 1;
+        }
+        
+        if (findSlope(dots[0], dots[3]) == findSlope(dots[1], dots[2])) {
+            return 1;
+        }
+        
+        return answer;
+    }
         
 
     
