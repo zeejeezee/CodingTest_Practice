83d8c6ee48e5423008e0fa46feb26d1b5f8cc74c
zeejeezee <jeezeedev@gmail.com>
2023-05-08 23:42:19 +0900

    5월 8일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex10.java
+++ src/main/java/programmers/lv0/ex10.java
@@ -324,6 +324,29 @@ public class ex10 {
         
         return answer;
     }
+    
+    
+    //겹치는 선분의 길이
+    //선분 3개가 평행하게 놓여 있습니다.
+    //세 선분의 시작과 끝 좌표가 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때,
+    //두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해 보세요.
+
+    //lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 선분이 두 개 이상 겹친 곳은 [-2, -1], [0, 1]로 길이 2만큼 겹쳐있습니다.
+    
+    public int solution10(int[][] lines) {
+        int[] arr = new int[200];
+        int answer = 0;
+        
+        for (int i = 0; i < lines.length; i++)
+            for (int j = lines[i][0] + 100; j < lines[i][1] + 100; j++)
+                arr[j]++;
+        
+        for (int i = 0; i < 200; i++)
+            if(arr[i] > 1)
+                answer++;
+        
+        return answer;
+    }
         
 
     
