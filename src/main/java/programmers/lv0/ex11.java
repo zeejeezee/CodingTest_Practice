ab1632b9c41f358864903764a6d0261eea05945f
zeejeezee <jeezeedev@gmail.com>
2023-05-11 23:55:37 +0900

    5월 11일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -11,14 +11,32 @@ public class ex11 {
     }
     
     
+    //소문자로 바꾸기
+    //알파벳으로 이루어진 문자열 myString이 주어집니다.
+    //모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+    
+    public String solution2(String myString) {
+        return myString.toLowerCase();
+    }
+    
+    
     //flag에 따라 다른 값 반환하기
     //두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
     //flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
     
     class Solution {
-        public int solution2(int a, int b, boolean flag) {
+        public int solution3(int a, int b, boolean flag) {
             return flag ? a + b : a - b;
         }
+    }
+    
+    
+    //정수 부분
+    //실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution4(double flo) {
+        return (int) flo;
+    }
         
 
     
