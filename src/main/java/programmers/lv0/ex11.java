f16202f8212090f637bd75dd9510e3f56b7fb6b4
zeejeezee <jeezeedev@gmail.com>
2023-05-14 00:20:36 +0900

    5월 14일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -24,10 +24,9 @@ public class ex11 {
     //두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
     //flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
     
-    class Solution {
-        public int solution3(int a, int b, boolean flag) {
-            return flag ? a + b : a - b;
-        }
+
+    public int solution3(int a, int b, boolean flag) {
+    	return flag ? a + b : a - b;
     }
     
     
@@ -54,7 +53,14 @@ public class ex11 {
     public int solution6(String n_str) {
         return Integer.parseInt(n_str);
     }
-        
+    
+    
+    //문자열로 변환
+    //정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해 주세요.
+    
+    public String solution7(int n) {
+        return Integer.toString(n);
+    }
 
     
 }
