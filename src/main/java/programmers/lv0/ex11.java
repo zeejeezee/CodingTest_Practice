e71888034a874dc85e51c2be2ced507ccdc655f3
zeejeezee <jeezeedev@gmail.com>
2023-05-15 23:19:23 +0900

    5월 15일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -61,6 +61,14 @@ public class ex11 {
     public String solution7(int n) {
         return Integer.toString(n);
     }
+    
+    
+    //n의 배수
+    //정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution8(int num, int n) {
+        return num % n == 0 ? 1 : 0;
+    }
 
     
 }
