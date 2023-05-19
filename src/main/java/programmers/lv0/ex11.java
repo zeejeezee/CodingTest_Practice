053fe2bf0bf5f632d5c103f06066ef44aaa59edf
zeejeezee <jeezeedev@gmail.com>
2023-05-19 10:39:02 +0900

    5월 19일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -8,6 +8,22 @@ public class ex11 {
     public String solution1(String my_string, int n) {
         return my_string.substring(0, n);
     }
+    
+    
+    //문자 리스트를 문자열로 변환하기
+    
+    //문자들이 담겨있는 배열 arr가 주어집니다. 
+    //arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+    
+    public String solution2(String[] arr) {
+        String answer = "";
+        
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        
+        return answer;
+    }
 
     
 }
