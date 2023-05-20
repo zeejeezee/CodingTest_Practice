eeb79e5bbd908e7591b56c48f34295d85db465d9
zeejeezee <jeezeedev@gmail.com>
2023-05-20 22:40:17 +0900

    5월 20일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -24,6 +24,26 @@ public class ex11 {
         
         return answer;
     }
+    
+    
+    //원소들의 곱과 합
+    //정수가 담긴 리스트 num_list가 주어질 때,
+    //모든 원소들의 곱이 모든 원소들의 합의 제곱보다 작으면 1을 크면 0을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution3(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        
+        for (int i = 0; i < num_list.length; i++){
+            sum += num_list[i];
+            mul *= num_list[i];
+        }
+        
+        if (sum * sum > mul) answer = 1;
+        
+        return answer;
+    }
 
     
 }
