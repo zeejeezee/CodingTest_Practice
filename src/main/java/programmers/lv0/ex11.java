156c751f38e54eb8c5618d5c3b95fb37bfb2aa87
zeejeezee <jeezeedev@gmail.com>
2023-05-22 19:30:27 +0900

    5월 22일 프로그래머스 코딩 테스트

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -53,6 +53,26 @@ public class ex11 {
     public String solution4(String my_string, String alp) {
         return my_string.replace(alp, alp.toUpperCase());
     }
-
     
+    
+    //조건에 맞게 수열 변환하기 3
+    //정수 배열 arr와 자연수 k가 주어집니다.
+    //만약 k가 홀수라면 arr의 모든 원소에 k를 곱하고, k가 짝수라면 arr의 모든 원소에 k를 더합니다.
+    //이러한 변환을 마친 후의 arr를 return 하는 solution 함수를 완성해 주세요.
+
+    public int[] solution5(int[] arr, int k) {
+        int[] answer = {};
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (k % 2 == 1) {
+                arr[i] *= k;
+            } else {
+                arr[i] += k;
+            }
+            
+            answer = arr;
+        }
+        
+        return answer;
+    }
 }
