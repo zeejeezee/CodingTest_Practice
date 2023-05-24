124cafaf6cb7d27beb233297252329fd0e24bf2d
zeejeezee <jeezeedev@gmail.com>
2023-05-24 22:22:44 +0900

    5월 24일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -75,4 +75,19 @@ public class ex11 {
         
         return answer;
     }
+    
+    
+    //n 번째 원소까지
+    //정수 리스트 num_list와 정수 n이 주어질 때,
+    //num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해 주세요.
+    
+    public int[] solution6(int[] num_list, int n) {
+        int[] answer = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = num_list[i];
+        }
+        
+        return answer;
+    }
 }
