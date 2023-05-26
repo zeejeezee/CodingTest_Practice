1b4cf4ba531a32240033a16e268e708ee14f55ce
zeejeezee <jeezeedev@gmail.com>
2023-05-26 11:15:31 +0900

    5월 26일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -107,4 +107,24 @@ public class ex11 {
         
         return answer;
     }
+    
+    
+    //첫 번째로 나오는 음수
+    //정수 리스트 num_list가 주어질 때, 첫 번째로 나오는 음수의 인덱스를 return하도록 solution 함수를 완성해 주세요.
+    //음수가 없다면 -1을 return 합니다.
+    
+    public int solution8(int[] num_list) {
+        int answer = 0;
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                answer = i;
+                break;
+            } else {
+                answer = -1;
+            }
+        }
+        
+        return answer;
+    }
 }
