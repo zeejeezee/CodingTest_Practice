921a84e9cbaa06ee24858b45ca56f68448b5d831
zeejeezee <jeezeedev@gmail.com>
2023-05-28 22:02:30 +0900

    5월 28일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -127,4 +127,21 @@ public class ex11 {
         
         return answer;
     }
+    
+    
+    //n보다 커질 때까지 구하기
+    //정수 배열 numbers와 정수 n이 매개변수로 주어집니다.
+    //numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간
+    //이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+    
+    public int solution9(int[] numbers, int n) {
+        int answer = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+            if(answer > n) break;
+        }
+        
+        return answer;
+    }
 }
