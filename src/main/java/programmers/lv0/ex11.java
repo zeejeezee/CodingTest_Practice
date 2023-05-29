c93a0f8b2b8071d896a8a33ff4e25aabb28d27b3
zeejeezee <jeezeedev@gmail.com>
2023-05-29 23:51:17 +0900

    5월 29일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex11.java
+++ src/main/java/programmers/lv0/ex11.java
@@ -144,4 +144,20 @@ public class ex11 {
         
         return answer;
     }
+    
+    
+    //글자 이어 붙여 문자열 만들기
+    //문자열 my_string과 정수 배열 index_list가 매개변수로 주어집니다.
+    //my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+    
+    public String solution10(String my_string, int[] index_list) {
+        String answer = "";
+        String[] my_strings = my_string.split("");
+        
+        for(int i = 0; i < index_list.length; i++) {
+            answer += my_strings[index_list[i]];
+        }
+        
+        return answer;
+    }
 }
