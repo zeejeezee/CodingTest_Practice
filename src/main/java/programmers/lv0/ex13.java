67cf833f9e461c90c7a461b00cb64c97e25720a2
zeejeezee <jeezeedev@gmail.com>
2023-05-31 22:37:24 +0900

    5월 31일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex13.java
+++ src/main/java/programmers/lv0/ex13.java
@@ -10,6 +10,22 @@ public class ex13 {
         String[] answer = my_string.split(" ");
         return answer;
     }
-
+    
+    
+    //배열에서 문자열 대소문자 변환하기
+    //문자열 배열 strArr가 주어집니다. 모든 원소가 알파벳으로만 이루어져 있을 때,
+    //배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로, 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
+    
+    public String[] solution2(String[] strArr) {
+        for (int i = 0; i < strArr.length; i++) {
+            if (i % 2 == 0) {
+                strArr[i] = strArr[i].toLowerCase();
+            } else {
+                strArr[i] = strArr[i].toUpperCase();
+            }
+        }
+        
+        return strArr;
+    }
     
 }
