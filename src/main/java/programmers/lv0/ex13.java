68b01b9562472f92ceedfc5b71a3ff133710ce6b
zeejeezee <jeezeedev@gmail.com>
2023-06-01 22:13:44 +0900

    6월 1일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex13.java
+++ src/main/java/programmers/lv0/ex13.java
@@ -28,4 +28,12 @@ public class ex13 {
         return strArr;
     }
     
+    
+    //문자열의 뒤의 n글자
+    //문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+    
+    public String solution3(String my_string, int n) {
+        return my_string.substring(my_string.length() - n);
+    }
+    
 }
