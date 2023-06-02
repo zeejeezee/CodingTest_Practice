0af492a79cd6b13aed6a1780d5117172eb9974fa
zeejeezee <jeezeedev@gmail.com>
2023-06-02 12:39:45 +0900

    6월 2일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex13.java
+++ src/main/java/programmers/lv0/ex13.java
@@ -36,4 +36,19 @@ public class ex13 {
         return my_string.substring(my_string.length() - n);
     }
     
+    
+    //부분 문자열
+    //어떤 문자열 A가 다른 문자열 B안에 속하면 A를 B의 부분 문자열이라고 합니다. 예를 들어 문자열 "abc"는 문자열 "aabcc"의 부분 문자열입니다.
+    //문자열 str1과 str2가 주어질 때, str1이 str2의 부분 문자열이라면 1을 부분 문자열이 아니라면 0을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution4(String str1, String str2) {
+        int answer = 0;
+        
+        if(str2.contains(str1)) {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+    
 }
