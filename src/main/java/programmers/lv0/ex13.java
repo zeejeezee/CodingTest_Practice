a8c56eff01222cdf8c3e4ea8298b1a3eb14d2c32
zeejeezee <jeezeedev@gmail.com>
2023-06-05 16:13:16 +0900

    6월 5일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex13.java
+++ src/main/java/programmers/lv0/ex13.java
@@ -86,4 +86,20 @@ public class ex13 {
     }
     
     
+    //문자열 정수의 합
+    //한 자리 정수로 이루어진 문자열 num_str이 주어질 때, 각 자리수의 합을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution7(String num_str) {
+        int answer = 0;
+        
+        String[] numArr = num_str.split("");
+        
+        for(String i : numArr) {
+            answer += Integer.valueOf(i);
+        }
+        
+        return answer;
+    }
+    
+    
 }
