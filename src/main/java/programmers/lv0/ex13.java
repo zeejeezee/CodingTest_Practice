f8e0094715b14f300e5767e3e839baa8cb3bbc82
zeejeezee <jeezeedev@gmail.com>
2023-06-07 22:24:13 +0900

    6월 7일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex13.java
+++ src/main/java/programmers/lv0/ex13.java
@@ -118,4 +118,20 @@ public class ex13 {
     }
     
     
+    //n개 간격의 원소들
+    //정수 리스트 num_list와 정수 n이 주어질 때,
+    //num_list의 첫 번째 원소부터 마지막 원소까지 n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해 주세요.
+    
+    public List solution(int[] num_list, int n) {
+        //굳이 배열 안 쓰고 리스트 써도 됨!!
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < num_list.length; i = i + n) {
+            answer.add(num_list[i]);
+        }
+        
+        return answer;
+    }
+    
+    
 }
