2d22aafd64265136961d2f5bd1affeb16c485d57
zeejeezee <jeezeedev@gmail.com>
2023-06-10 20:44:01 +0900

    6월 10일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex14.java
+++ src/main/java/programmers/lv0/ex14.java
@@ -1,6 +1,7 @@
 package programmers.lv0;
 
 public class ex14 {
+	
 	//마지막 두 원소
 	//정수 리스트 num_list가 주어질 때,
 	//마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을,
@@ -22,5 +23,33 @@ public class ex14 {
         
         return answer;
     }
-
+    
+    
+    //수 조작하기 1
+    //정수 n과 문자열 control이 주어집니다.
+    //control은 "w", "a", "s", "d"의 4개의 문자로 이루어져 있으며, control의 앞에서부터 순서대로 문자에 따라 n의 값을 바꿉니다.
+    //"w" : n이 1 커집니다.
+    //"s" : n이 1 작아집니다.
+    //"d" : n이 10 커집니다.
+    //"a" : n이 10 작아집니다.
+    //위 규칙에 따라 n을 바꿨을 때 가장 마지막에 나오는 n의 값을 return 하는 solution 함수를 완성해 주세요.
+    
+    public int solution2(int n, String control) {
+        int answer = n;
+        String[] arr = control.split("");
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i].equals("w")) {
+                answer += 1;   
+            } else if (arr[i].equals("s")) {
+                answer -= 1;
+            } else if (arr[i].equals("d")) {
+                answer += 10;
+            } else if (arr[i].equals("a")) {
+                answer -= 10;
+            }
+        }
+        
+        return answer;
+    }
 }
