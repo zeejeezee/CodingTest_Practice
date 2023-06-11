097f8d03d98dcc72016dd19394284d40cbd5b7ea
zeejeezee <jeezeedev@gmail.com>
2023-06-11 14:53:55 +0900

    6월 11일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex14.java
+++ src/main/java/programmers/lv0/ex14.java
@@ -1,5 +1,7 @@
 package programmers.lv0;
 
+import java.util.Scanner;
+
 public class ex14 {
 	
 	//마지막 두 원소
@@ -52,4 +54,18 @@ public class ex14 {
         
         return answer;
     }
+    
+    
+    //문자열 붙여서 출력하기
+    //두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+    //입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.print(a + b);
+    }
+    
 }
