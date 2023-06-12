a3430b35437528417958ec1be7b91c16bd9c75f8
zeejeezee <jeezeedev@gmail.com>
2023-06-13 00:11:02 +0900

    6월 13일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex14.java
+++ src/main/java/programmers/lv0/ex14.java
@@ -78,4 +78,20 @@ public class ex14 {
         return answer;
     }
     
+    
+    //홀짝 구분하기
+    //자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+    
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if (n % 2 == 0) {
+            System.out.print(n + " is even");
+        } else {
+            System.out.print(n + " is odd");
+        }
+    }
+    
+    
 }
