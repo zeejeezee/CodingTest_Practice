b23de54887b8c20b41530f0c49aff7f08ceaf9b5
zeejeezee <jeezeedev@gmail.com>
2023-06-17 17:13:07 +0900

    6월 17일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex14.java
+++ src/main/java/programmers/lv0/ex14.java
@@ -145,6 +145,30 @@ public class ex14 {
         
         return answer;
     }
+    
+    
+    //배열의 길이에 따라 다른 연산하기
+    //정수 배열 arr과 정수 n이 매개변수로 주어집니다.
+    //arr의 길이가 홀수라면 arr의 모든 짝수 인덱스 위치에 n을 더한 배열을,
+    //arr의 길이가 짝수라면 arr의 모든 홀수 인덱스 위치에 n을 더한 배열을 return 하는 solution 함수를 작성해 주세요.
+    
+    public int[] solution9(int[] arr, int n) {
+        int[] answer = arr;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if (arr.length % 2 != 0) {
+                if (i % 2 == 0) {
+                    answer[i] = arr[i] + n;
+                }
+            } else {
+                if (i % 2 != 0) {
+                    answer[i] = arr[i] + n;
+                }
+            }
+        }
+        
+        return answer;
+    }
 
 
     
