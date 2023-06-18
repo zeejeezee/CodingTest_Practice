f1fc4d878bf57cd0b76cdf9228450b1d76077fe4
zeejeezee <jeezeedev@gmail.com>
2023-06-18 18:16:59 +0900

    6월 18일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex14.java
+++ src/main/java/programmers/lv0/ex14.java
@@ -169,6 +169,22 @@ public class ex14 {
         
         return answer;
     }
+    
+    
+    //카운트 다운
+    //정수 start와 end가 주어질 때,
+    //start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해 주세요.
+    
+    public int[] solution10(int start, int end) {
+        int[] answer = new int[start - end + 1];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start--;
+        }
+
+        return answer;
+    }
+    
 
 
     
