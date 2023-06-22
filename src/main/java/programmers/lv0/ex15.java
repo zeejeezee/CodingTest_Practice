325ba207c0bae4bb1c6b807e195016fada503d02
zeejeezee <jeezeedev@gmail.com>
2023-06-22 23:24:09 +0900

    6월 22일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex15.java
+++ src/main/java/programmers/lv0/ex15.java
@@ -55,6 +55,27 @@ public class ex15 {
         
         return answer;
     }
+    
+    
+    //이어 붙인 수
+    //정수가 담긴 리스트 num_list가 주어집니다.
+    //num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해 주세요.
+    
+    public int solution4(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0) //짝수일 경우
+                even += Integer.toString(num_list[i]);
+            else odd += Integer.toString(num_list[i]);
+        }
+        
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        
+        return answer;
+    }
 
 
     
