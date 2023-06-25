310e6a86e23608824db5e66ffce28f3a09c50d01
zeejeezee <jeezeedev@gmail.com>
2023-06-25 21:36:50 +0900

    6월 25일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex15.java
+++ src/main/java/programmers/lv0/ex15.java
@@ -122,6 +122,26 @@ public class ex15 {
         
         return answer;
     }
+    
+    
+    //꼬리 문자열
+    //문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열이라고 합니다.
+    //꼬리 문자열을 만들 때 특정 문자열을 포함한 문자열은 제외시키려고 합니다.
+    //예를 들어 문자열 리스트 ["abc", "def", "ghi"]가 있고 문자열 "ef"를 포함한 문자열은 제외하고 꼬리 문자열을 만들면 "abcghi"가 됩니다.
+    //문자열 리스트 str_list와 제외하려는 문자열 ex가 주어질 때,
+    //str_list에서 ex를 포함한 문자열을 제외하고 만든 꼬리 문자열을 return하도록 solution 함수를 완성해 주세요.
+    
+    public String solution7(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (String str : str_list) {
+            if (!str.contains(ex)) { //str이 ex를 포함하지 않는다면
+                sb.append(str); //해당 문자열을 StringBuilder에 추가
+            }
+        }
+        
+        return sb.toString();
+    }
 
     
 }
