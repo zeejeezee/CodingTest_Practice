45ce900a11f83ccad1221de15048bf088f793d2a
zeejeezee <jeezeedev@gmail.com>
2023-06-27 23:27:59 +0900

    6월 27일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex15.java
+++ src/main/java/programmers/lv0/ex15.java
@@ -159,6 +159,29 @@ public class ex15 {
         
         return answer;
     }
+    
+    
+    //홀수 vs 짝수
+    //정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때, 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해 주세요.
+    //두 값이 같을 경우 그 값을 return합니다.
+    
+    public int solution9(int[] num_list) {
+        int answer = 0;
+        int even = 0;
+        int odd = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+        
+        answer = even > odd ? even : odd;
+        
+        return answer;
+    }
 
     
 }
