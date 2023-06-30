6c566e461dde9b3ee1db6b58c642cb1aed05c7ca
zeejeezee <jeezeedev@gmail.com>
2023-06-30 19:39:19 +0900

    6월 30일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex16.java
+++ src/main/java/programmers/lv0/ex16.java
@@ -23,4 +23,28 @@ public class ex16 {
         
         return answer;
     }
+    
+    
+    //더 크게 합치기
+    //연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+	//12 ⊕ 3 = 123
+	//3 ⊕ 12 = 312
+	//양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+	
+	//단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+    
+    public int solution2(int a, int b) {
+        String stringA = String.valueOf(a);
+        String stringB = String.valueOf(b);
+        
+        String strSum1 = stringA + stringB;
+        String strSum2 = stringB + stringA;
+        
+        return Math.max(Integer.valueOf(strSum1), Integer.valueOf(strSum2));
+    }
+    
+    
+    
+    
 }
