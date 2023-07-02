3410ba64cffd8e289c98b4f3a40cdd668770e090
zeejeezee <jeezeedev@gmail.com>
2023-07-02 21:42:09 +0900

    7월 2일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex16.java
+++ src/main/java/programmers/lv0/ex16.java
@@ -1,6 +1,7 @@
 package programmers.lv0;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ex16 {
@@ -79,6 +80,16 @@ public class ex16 {
     
     public String solution3_2(String myString) {
         return myString.replaceAll("[a-l]", "l");
+    }
+    
+    
+    //뒤에서 5등까지
+    //정수로 이루어진 리스트 num_list가 주어집니다.
+    //num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해 주세요.
+    
+    public int[] solution4(int[] num_list) {
+        Arrays.sort(num_list);
+        return Arrays.copyOf(num_list, 5);
     }
     
     
