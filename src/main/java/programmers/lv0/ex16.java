bc1abfcda445d3e4250f14d8adbbe8bca8e5656c
zeejeezee <jeezeedev@gmail.com>
2023-07-03 23:31:56 +0900

    7월 3일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex16.java
+++ src/main/java/programmers/lv0/ex16.java
@@ -93,6 +93,14 @@ public class ex16 {
     }
     
     
+    //0 떼기
+    //정수로 이루어진 문자열 n_str이 주어질 때, n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해 주세요.
+    
+    public String solution5(String n_str) {
+        return "" + Integer.parseInt(n_str);
+    }
+    
+    
     
     
 }
