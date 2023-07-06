7df86ff9c8acc7248d614c1d25e663edf77cf9c9
zeejeezee <jeezeedev@gmail.com>
2023-07-06 13:42:21 +0900

    7월 6일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex16.java
+++ src/main/java/programmers/lv0/ex16.java
@@ -166,5 +166,26 @@ public class ex16 {
     }
     
     
+    //홀짝에 따라 다른 값 반환하기
+    //양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고
+    //n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+    
+    public int solution9(int n) {
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % 2 != 0) {
+                if (i % 2 != 0) { //홀수일 때
+                    answer += i;
+                }
+            } else {
+                if (i % 2 == 0) { //짝수일 때
+                    answer += i * i;
+                }
+            }
+        }
+        
+        return answer;
+    }
     
 }
