c13b98be822191f15d136a9a6605c85da98bac56
zeejeezee <jeezeedev@gmail.com>
2023-07-12 12:52:30 +0900

    7월 12일 프로그래머스 코딩 테스트 연습

--- src/main/java/programmers/lv0/ex17.java
+++ src/main/java/programmers/lv0/ex17.java
@@ -82,4 +82,30 @@ public class ex17 {
     }
     
     
+    //간단한 식 계산하기
+    //문자열 binomial이 매개변수로 주어집니다.
+    //binomial은 "a op b" 형태의 이항식이고 a와 b는 음이 아닌 정수, op는 '+', '-', '*' 중 하나입니다.
+    //주어진 식을 계산한 정수를 return 하는 solution 함수를 작성해 주세요.
+    
+    public int solution5(String binomial) {
+        int answer = 0;
+        
+        String[] answerArr = binomial.split(" ");
+        
+        int a = Integer.parseInt(answerArr[0]);
+        int b = Integer.parseInt(answerArr[2]);
+        String op = answerArr[1];
+        
+        if (op.equals("+")) {
+            answer = a + b;
+        } else if (op.equals("-")) {
+            answer = a - b;
+        } else {
+            answer = a * b;
+        }
+        
+        return answer;
+    }
+    
+    
 }
